afdb77441732f0a7bbf90c5b09a91b2fa51bee6a
Juan Carlos Carbajal Ipenza <goesrex@gmail.com>
2016-09-12 02:01:21 -0500

    Clase "Una pequeña revisión".

---
+++ Swift-programar-para-iOS_Semana-5_MasFeliz/MyPlayground.playground/Contents.swift
@@ -0,0 +1,22 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+// variables y constantes
+var nombre = "David"
+let edad = 40
+
+// arreglos
+var deportes = ["Soccerr"]
+deportes.append("Natación")
+deportes.append("Golf")
+
+deportes[2]
+
+// opcionales
+var apellido: String? = "Escarcega"
+
+apellido = nil
