2a3ae3173470ac266b39c53bb6baaa9f55ee2fce
Juan Carlos Carbajal Ipenza <goesrex@gmail.com>
2016-09-12 03:09:59 -0500

    Clase "Estructura con colores".

---
+++ Swift-programar-para-iOS_Semana-5_MasFeliz/ColoresStruct.swift
@@ -0,0 +1,27 @@
+//
+//  ColoresStruct.swift
+//  Swift-programar-para-iOS_Semana-5_MasFeliz
+//
+//  Created by Juan Carlos Carbajal Ipenza on 12/09/16.
+//  Copyright © 2016 Juan Carlos Carbajal Ipenza. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Colores {
+    let colores = [
+        UIColor(red: 210/255, green: 90/255, blue: 45/255, alpha: 1),
+        UIColor(red: 40/255, green: 170/255, blue: 45/255, alpha: 1),
+        UIColor(red: 3/255, green: 180/255, blue: 90/255, alpha: 1),
+        UIColor(red: 210/255, green: 190/255, blue: 5/255, alpha: 1),
+        UIColor(red: 120/255, green: 120/255, blue: 50/255, alpha: 1),
+        UIColor(red: 130/255, green: 80/255, blue: 90/255, alpha: 1),
+        UIColor(red: 130/255, green: 130/255, blue: 130/255, alpha: 1),
+        UIColor(red: 3/255, green: 50/255, blue: 90/255, alpha: 1)]
+    
+    func regresaColorAleatorio() -> UIColor {
+        let posicion = Int(arc4random()) % colores.count
+        return colores[posicion]
+    }
+}
--- Swift-programar-para-iOS_Semana-5_MasFeliz/MyPlayground.playground/Contents.swift
+++ Swift-programar-para-iOS_Semana-5_MasFeliz/MyPlayground.playground/Contents.swift
@@ -20,3 +20,20 @@ deportes[2]
 var apellido: String? = "Escarcega"
 
 apellido = nil
+
+let color = UIColor(red: 30/255, green: 180/255, blue: 20/255, alpha: 1)
+
+arc4random() % 100
+
+
+/*
+let colores = [
+UIColor(red: 210/255, green: 90/255, blue: 45/255, alpha: 1),
+UIColor(red: 40/255, green: 170/255, blue: 45/255, alpha: 1),
+UIColor(red: 3/255, green: 180/255, blue: 90/255, alpha: 1),
+UIColor(red: 210/255, green: 190/255, blue: 5/255, alpha: 1),
+UIColor(red: 120/255, green: 120/255, blue: 50/255, alpha: 1),
+UIColor(red: 130/255, green: 80/255, blue: 90/255, alpha: 1),
+UIColor(red: 130/255, green: 130/255, blue: 130/255, alpha: 1),
+UIColor(red: 3/255, green: 50/255, blue: 90/255, alpha: 1)]
+ */
